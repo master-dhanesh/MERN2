393225ee698c945a928e4c6e91be8c05a94ec27e
master-dhanesh <dhanesh1296@gmail.com>
2022-01-18 15:00:55 +0530

    JS DAY REVISION 01

---
+++ script.js
@@ -0,0 +1,136 @@
+// Array
+arr = [1,2,3,4,5,6]
+// arr[0] =100
+// push unshift 
+// pop shift splice
+// for (const i of arr) {
+//     console.log(i*2)
+// }  
+
+// arr.forEach(function(e){
+//     console.log(e)
+// });
+
+// s = arr.map(function(e){
+//    return e*2
+// })
+// console.log(arr, s)
+
+
+// console.log(arr)
+
+
+
+// Objects
+// obj = {
+//     name: 'John Doe',
+//     skills: "Full Stack Developer",
+//     education: "Post-Grad"
+// }
+// obj.city = "Bhopal"
+// obj.name = "Anney"
+// delete obj["name"]
+// console.log(obj)
+// for (const key in obj) {
+// console.log(key, obj[key])
+// }
+
+
+
+// Switch
+    // let i = 12 //1,2,3
+    // switch (i) {
+    //     case 1:
+    //     case 2:
+    //     case 10:
+    //     case 11:
+    //     case 12:
+    //         console.log("one")
+    //         break;
+    //     case 2:
+    //         console.log("two")
+    //         break;
+    //     case 3:
+    //         console.log("three")
+    //         break;
+    //     default:
+    //         console.log("Invalid Input")
+    //         break;
+    // }
+
+// Functions
+    // parametrised/non-para
+    // print/return
+    // pure/impure
+    // let a = 8
+    // function funname(para1){
+    //     body of funtion
+    //     return ...
+    // }
+
+// Loops
+    // loop variable / loop init
+    // test exp. loop
+    // updation exp.
+    // entry control 
+        // for 
+        // for (let i = 0; i < 10; i++) {
+            // loop body
+        // }  
+        // while
+        // let i = 0 
+        // while (i <10) {
+        //     loop body
+        //     updation exp i++
+        // }
+    //exit control
+        //  do while
+        // let i = 0
+        // do {
+        //     loop body
+        //     i++
+        // } while (i < 10);
+    
+    
+// If-else
+    // simple if
+    // simple If-else
+    // multiple if-else
+    // nested if-else
+    // if(condition){
+    //     // if body
+    //     if(condition){
+    //         // body
+    //     }
+    // } else if(condition) {
+    //     // else body
+    // } else {
+    //     // else body 
+    // }
+
+// Operators
+    // Assignmental -> =
+    // Arith -> + - / * % **
+    // Comparision -> > >= < <= == === != !== (true/false)
+    // Logical -> && || !
+    // increment/decrement -> post/ pre  -> --i ++i
+    // console.log(!(false || 0 || 34))
+    // console.log(false && 0 && 34)
+    
+// Datatypes
+    // Number 
+    // String 
+    // Boolean -> falsy-> 0 Null undefined false ''
+    // null -> datatype(object)
+    // undefined
+    // Object -> object{}/array[]
+    // Symbol (Homework)
+
+
+// Variable
+    // var
+    // const 
+    // let
+
+
+
