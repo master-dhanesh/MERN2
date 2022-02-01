30bfbcb27eada597d828871961da3ad754d3989e
master-dhanesh <dhanesh1296@gmail.com>
2022-02-01 15:07:50 +0530

    DYNAMIC JS 01

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ signinbtn.addEventListener('click', function(e) {
     
 })
 
+// --------------------------------------------------------------
+
+const signupform = document.querySelector('.signupform');
+const second = document.querySelector('.second');
+
+const Users = [];
+
+
+function Add(u) {
+    second.innerHTML = ''
+    u.forEach(e => {
+        second.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${e.name}</h5>
+          <p class="card-text">${e.email}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`
+    })
+
+}
+
+    
+    
+
+
+signupform.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let {email, name, password} = e.target
+
+    let data = {
+        email: email.value,
+        name: name.value,
+        password: password.value,
+    }
+
+    Users.push(data);
+    
+    email.value = '';
+    name.value = '';
+    password.value = '';
+    console.log(Users);
+
+    Add(Users)
+});
