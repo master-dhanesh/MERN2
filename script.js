7da87b4d460bc338b6d98d804680f087ebda27b4
master-dhanesh <dhanesh1296@gmail.com>
2022-02-02 14:58:04 +0530

    DYNAMIC JS CLASS 02

--- script.js
+++ script.js
@@ -1,65 +1,38 @@
-const signupbtn = document.querySelector('.signupbtn')
-const signinbtn = document.querySelector('.signinbtn')
-const myForm = document.querySelector('.myForm');
+const form = document.querySelector('form');
+const Cards = document.querySelector('.cards-container')
 
+const CardsArray = [];
 
-signupbtn.addEventListener('click', function(e) {
-    myForm.style.transform = 'translate(0%, 0)'
-    signinbtn.classList.remove('borderbottom');
-    signupbtn.classList.add('borderbottom');
-})
+AddCard(CardsArray);
 
-signinbtn.addEventListener('click', function(e) {
-    myForm.style.transform = 'translate(-50%, 0)';
-    signinbtn.classList.add('borderbottom');
-    signupbtn.classList.remove('borderbottom');
-    
-})
-
-// --------------------------------------------------------------
-
-const signupform = document.querySelector('.signupform');
-const second = document.querySelector('.second');
-
-const Users = [];
-
-
-function Add(u) {
-    second.innerHTML = ''
-    u.forEach(e => {
-        second.innerHTML += `<div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+function AddCard(cards) {
+    Cards.innerHTML = '';
+    cards.forEach(function (card) {
+        Cards.innerHTML += `
+        <div class="card me-3" style="width: 15.5rem;">
+        <img src="${card.avatar}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${e.name}</h5>
-          <p class="card-text">${e.email}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p class="card-text">
+            ${card.name} <br>
+            ${card.contact} <br>
+            ${card.message}
+        </p>
         </div>
-      </div>`
+    </div>
+        `
     })
-
 }
 
-    
-    
-
-
-signupform.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-
-    let {email, name, password} = e.target
-
-    let data = {
-        email: email.value,
+    const { name, contact, avatar, message } = e.target;
+    const newCard = {
         name: name.value,
-        password: password.value,
+        contact: contact.value,
+        avatar: avatar.value,
+        message: message.value
     }
+    CardsArray.push(newCard);
 
-    Users.push(data);
-    
-    email.value = '';
-    name.value = '';
-    password.value = '';
-    console.log(Users);
-
-    Add(Users)
-});
+    AddCard(CardsArray);
+})
