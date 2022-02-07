eb6814a32174c1f557f1e3d5b55560e75c8e7795
master-dhanesh <dhanesh1296@gmail.com>
2022-02-07 14:50:47 +0530

    GSAP 01

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
+let tl = gsap.timeline({
+        defaults: {duration: .5},
+        yoyo:true,
+        // repeat: -1,
+        onRepeat: ()=> console.log('repeating...'),
+        onUpdate: ()=> console.log('updating...'),
+        onComplete: ()=> console.log('complete...'),
+        onStart: ()=> console.log('start...'),
+    });
 
-listItems = []
-
-function addlist(alllist){
-    ul.innerHTML = "";
-    alllist.forEach(function(l,i){
-        ul.innerHTML +=  `<li>${l.name} - ${l.city} - <button onClick="removeEl(${i})">Remove</button></li>`
-    })
-}
-
-function removeEl(i){
-    listItems.splice(i, 1);
-    addlist(listItems);
-}
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const { name, city } = e.target;
-    const newList = {
-        name: name.value,
-        city: city.value
-    };
-    
-    listItems.push(newList);
-    addlist(listItems);
-    name.value = "";
-    city.value = "";
-});
+tl.to('.box1', {y:100})
+.to('.box2', {y:-100})
+.to('.box3', {scale: 0})
+.to('.box4', {scale: 2, opacity: 0})
 
 
-function showDialog(){
- alert("Hellooo?")
-}
+
+
+// gsap.set('.box1', {y: -200} )
+
+// var tween = gsap.to('.box',  {
+//     repeat: -1,
+//     yoyo: true,
+//     y: 100,
+//     delay: 1,
+//     duration: .5,
+//     ease: "bounce",
+//     stagger: .2,
+    // stagger: {
+    //     amount: .2,
+    //     grid: 'auto',
+    //     from: 'left'
+    // }
+// })
+
+// function util(){
+//     tween.timeScale(2);
+// }
+
+// gsap.from('.box1',{
+//     opacity: 0,
+//     x: -200,
+//     scale: 0,
+//     delay: 1,
+//     duration: .5
+// })
+
+// gsap.fromTo(".box1", {
+//     opacity: 0,
+//     x: -200,
+//     scale: 0,
+//     // delay: 1,
+//     // duration: .5
+// }, {
+//     opacity: 1,
+//     x: 0,
+//     scale: 1,
+//     delay: 1,
+//     duration: 1,
+//     ease: "slow"
+// });
