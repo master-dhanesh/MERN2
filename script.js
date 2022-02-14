348567ade773c28e9fa014ad195f554325c112b4
master-dhanesh <dhanesh1296@gmail.com>
2022-02-14 15:36:40 +0530

    GASP 02 SCROLLTRIGGER

--- script.js
+++ script.js
@@ -1,61 +1,16 @@
-let tl = gsap.timeline({
-        defaults: {duration: .5},
-        yoyo:true,
-        // repeat: -1,
-        onRepeat: ()=> console.log('repeating...'),
-        onUpdate: ()=> console.log('updating...'),
-        onComplete: ()=> console.log('complete...'),
-        onStart: ()=> console.log('start...'),
-    });
+gsap.registerPlugin(ScrollTrigger);
 
-tl.to('.box1', {y:100})
-.to('.box2', {y:-100})
-.to('.box3', {scale: 0})
-.to('.box4', {scale: 2, opacity: 0})
-
-
-
-
-// gsap.set('.box1', {y: -200} )
-
-// var tween = gsap.to('.box',  {
-//     repeat: -1,
-//     yoyo: true,
-//     y: 100,
-//     delay: 1,
-//     duration: .5,
-//     ease: "bounce",
-//     stagger: .2,
-    // stagger: {
-    //     amount: .2,
-    //     grid: 'auto',
-    //     from: 'left'
-    // }
-// })
-
-// function util(){
-//     tween.timeScale(2);
-// }
-
-// gsap.from('.box1',{
-//     opacity: 0,
-//     x: -200,
-//     scale: 0,
-//     delay: 1,
-//     duration: .5
-// })
-
-// gsap.fromTo(".box1", {
-//     opacity: 0,
-//     x: -200,
-//     scale: 0,
-//     // delay: 1,
-//     // duration: .5
-// }, {
-//     opacity: 1,
-//     x: 0,
-//     scale: 1,
-//     delay: 1,
-//     duration: 1,
-//     ease: "slow"
-// });
+gsap.to('.box2', {
+    // scrollTrigger: '.box3',
+    scrollTrigger: {
+        trigger: '.box1',
+        start: 'center 20%',
+        endTrigger: '.box3',
+        end: 'top 80%',
+        markers: true,
+        toggleActions: 'restart pause reverse pause',
+    },
+    x: 400,
+    rotation: 360,
+    duration: 3
+})
