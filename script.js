5303f5cf7aeb748a820f498a77d70b6da60e4396
master-dhanesh <dhanesh1296@gmail.com>
2022-02-15 15:20:52 +0530

    GSAP 03 DAY

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ease: 'none', duration:2});
 
-gsap.to('.box2', {
-    // scrollTrigger: '.box3',
-    scrollTrigger: {
-        trigger: '.box1',
-        start: 'center 20%',
-        endTrigger: '.box3',
-        end: 'top 80%',
-        markers: true,
-        toggleActions: 'restart pause reverse pause',
-    },
-    x: 400,
-    rotation: 360,
-    duration: 3
+gsap.utils.toArray('.box').forEach(b => {
+    ScrollTrigger.create({
+    trigger: b,
+    start: 'top top',
+    scrub: true,
+    pin: true,
+    pinSpacing: false,
+    markers: true
+})
 })
