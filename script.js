aa15f61269fd865311386fe09c6781c33d279925
master-dhanesh <dhanesh1296@gmail.com>
2022-02-24 15:43:15 +0530

    PROJECT DAY 01

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.defaults({ease: 'none', duration:2});
 
-gsap.utils.toArray('.box').forEach(b => {
-    ScrollTrigger.create({
-    trigger: b,
-    start: 'top top',
-    scrub: true,
-    pin: true,
-    pinSpacing: false,
-    markers: true
-})
-})
+// forEach 
+// map filter 
+let arr = [1,2,3,4,5] 
+// 
+let newArr = arr.reduce((acc, cv) => acc + cv, 0);
+
+console.log(newArr, arr);
